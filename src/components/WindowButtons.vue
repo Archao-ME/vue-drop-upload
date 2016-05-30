e505372e9fef139a11aebd74cbe1b266d8927ef9
Jesusalexander <brpoper@gmail.com>
2016-05-31 03:35:43 +0800

    base scss color list/close buttom/itemInfo/drop window

---
+++ src/components/WindowButtons.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="window-buttons">
+    <ul class="window-buttons-ul">
+      <li class="close-button" v-on:click = "onClose()"></li>
+      <li class="min-button" v-on:click = "onMin()"></li>
+      <li class="zoom-button" v-on:click = "onZoom()"></li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    onClose: function () {
+      this.$dispatch('onCloseWindow', true)
+    }
+  }
+}
+</script>
+<style lang="scss">
+@import "../assets/base.scss";
+.window-buttons-ul{
+  li {
+    width: 10px;
+    height: 10px;
+    border-radius: 5px;
+    margin: 0 5px;
+    padding: 0;
+    overflow: hidden;
+    float: left;
+  }
+  li.close-button {
+    background-color: $alizarn;
+  }
+  li.min-button {
+    background-color: $sunFlower;
+  }
+  li.zoom-button {
+    background-color: $emerald;
+  }
+}
+.close-button{
+}
+</style>
---
+++ src/components/imageUploader/ItemShow.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="show-wrapper" v-show="isShow">
+    <div class="name">
+      <p>{{ name }}</p>
+    </div>
+    <div class="url">
+      <p>{{ url }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    name: String,
+    url: String,
+    info: String,
+    isShow: Boolean
+  }
+}
+</script>
+<style lang="scss">
+@import "../../assets/base.scss";
+.show-wrapper{
+  line-height: 30px;
+  color: $clouds;
+  font-style: Montserrat;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  background: $clouds;
+  z-index: 1100;
+  box-shadow:0 1px 1px rgba(0, 0, 0, 0.7);
+}
+.show-wrapper::after,
+.show-wrapper::before {
+  content:"";
+  position:relative;
+  z-index:-1;
+  bottom:15px;
+  left:5px;
+  width:50%;
+  height:20%;
+  box-shadow:0 15px 10px rgba(0, 0, 0, 0.7);
+  transform:rotate(-3deg);
+}
+.show-wrapper::before {
+  transform:rotate(2deg);
+  left:auto;
+}
+.name,.url {
+  overflow: hidden;
+}
+.name{
+  background-color: $peterRiver;
+  height: 25px;
+  font-size: 14px;
+}
+.url{
+  background-color: $belizeHole;
+  height: 25px;
+  font-size: 12px;
+}
+</style>
