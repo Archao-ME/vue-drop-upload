64bee5c4573cfe937bf854e974da0131877cb4bc
Jesusalexander <brpoper@gmail.com>
2016-05-31 20:07:41 +0800

    0.01

--- electron.entry.js
+++ electron.entry.js
@@ -19,7 +19,7 @@ ipcMain.on('clipboard-write', function (event,msg) {
 });
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({width: 450, height: 600});
+  win = new BrowserWindow({width: 450, height: 600,resizable: false,frame: false,transparent:true});
 
   // and load the index.html of the app.
   win.loadURL(`file://${__dirname}/dist/index.html`);
